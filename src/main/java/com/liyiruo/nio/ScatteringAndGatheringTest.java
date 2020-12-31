b1b0b6f547a394213ccd73e7a780ccc4f8b7ca19
liyiruo <401213245@qq.com>
2021-01-01 00:30:09 +0800

    修改一个文件

--- src/main/java/com/liyiruo/nio/ScatteringAndGatheringTest.java
+++ src/main/java/com/liyiruo/nio/ScatteringAndGatheringTest.java
@@ -1,12 +1,55 @@
 package com.liyiruo.nio;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Arrays;
+
 /**
  * @author liyiruo
  * @Description
  * @Date 2020/12/31 下午10:57
  */
 public class ScatteringAndGatheringTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //使用serverSocketChannel 和socketchannel 网络
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
 
+        InetSocketAddress inetSocketAddress = new InetSocketAddress(7000);
+
+        //绑定端口到socket，并启动
+
+        serverSocketChannel.socket().bind(inetSocketAddress);
+
+        ByteBuffer[] byteBuffers = new ByteBuffer[2];
+        byteBuffers[0] = ByteBuffer.allocate(5);
+        byteBuffers[1] = ByteBuffer.allocate(3);
+
+
+        SocketChannel socketChannel = serverSocketChannel.accept();
+        int messageLength = 8;
+
+        while (true) {
+            int byteRead = 0;
+            while (byteRead < messageLength) {
+                long l = socketChannel.read(byteBuffers);
+                byteRead += 1;
+                System.out.println("byteRead=" + byteRead);
+                Arrays.asList(byteBuffers).stream().map(buffer -> "postion=" + buffer.position() + ",limit=" + buffer.limit()).
+                        forEach(System.out::println);
+            }
+            Arrays.asList(byteBuffers).forEach(buffer -> buffer.flip());
+            long byteWrite = 0;
+            while (byteWrite < messageLength) {
+                long l = socketChannel.write(byteBuffers);
+                byteWrite += 1;
+            }
+            //将所有的buffer 进行clear
+            Arrays.asList(byteBuffers).forEach(bufer->{bufer.clear();});
+
+            System.out.println("byteRead:="+byteRead+" byteWrite="+byteWrite+",messagelength=>:"+messageLength);
+        }
     }
 }
